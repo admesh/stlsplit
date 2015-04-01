5ba4bf083040501746f08d4b20315445fe8eae79
Miro Hrončok <miro@hroncok.cz>
2015-04-01 22:20:11 +0200

    Provide stl_split function

--- stlsplit.cpp
+++ stlsplit.cpp
@@ -21,25 +21,19 @@
 #include <sstream>
 #include <admesh/stl.h>
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " file.stl" << std::endl;
-    return 1;
-  }
-  stl_file stl_in;
-  stl_open(&stl_in, argv[1]);
-  stl_exit_on_error(&stl_in);
+std::vector<stl_file*> stl_split(stl_file* stl_in) {
   // We need neighbors and filled holes
-  stl_repair(&stl_in, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 /* fill_holes_flag */, 0, 0, 0, 0);
+  stl_repair(stl_in, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 /* fill_holes_flag */, 0, 0, 0, 0);
 
   std::set<int> seen_facets;
+  std::vector<stl_file*> parts;
   
   // loop while we have remaining facets
-  for (unsigned int namecounter = 1; ; namecounter++) {
+  for (;;) {
     // get the first facet
     std::queue<int> facet_queue;
     std::deque<int> facets;
-    for (int facet_idx = 0; facet_idx < stl_in.stats.number_of_facets; facet_idx++) {
+    for (int facet_idx = 0; facet_idx < stl_in->stats.number_of_facets; facet_idx++) {
       if (seen_facets.find(facet_idx) == seen_facets.end()) {
         // if facet was not seen put it into queue and start searching
         facet_queue.push(facet_idx);
@@ -47,8 +41,7 @@ int main(int argc, char **argv) {
       }
     }
     if (facet_queue.empty()) {
-      stl_close(&stl_in);
-      return 0;
+      return parts;
     }
     
     while (!facet_queue.empty()) {
@@ -57,33 +50,55 @@ int main(int argc, char **argv) {
       if (seen_facets.find(facet_idx) != seen_facets.end()) continue;
       facets.push_back(facet_idx);
       for (int j = 0; j <= 2; j++) {
-        facet_queue.push(stl_in.neighbors_start[facet_idx].neighbor[j]);
+        facet_queue.push(stl_in->neighbors_start[facet_idx].neighbor[j]);
       }
       seen_facets.insert(facet_idx);
     }
     
-    stl_file stl_out;
-    stl_out.stats.type = inmemory;
-    stl_out.stats.number_of_facets = facets.size();
-    stl_out.stats.original_num_facets = stl_out.stats.number_of_facets;
-    stl_out.v_indices = NULL;
-    stl_out.v_shared = NULL;
-    stl_out.neighbors_start = NULL;
-    stl_clear_error(&stl_out);
-    stl_allocate(&stl_out);
+    stl_file *stl_out = new stl_file;
+    stl_out->stats.type = inmemory;
+    stl_out->stats.number_of_facets = facets.size();
+    stl_out->stats.original_num_facets = stl_out->stats.number_of_facets;
+    stl_out->v_indices = NULL;
+    stl_out->v_shared = NULL;
+    stl_out->neighbors_start = NULL;
+    stl_clear_error(stl_out);
+    stl_allocate(stl_out);
+    
     
     int first = 1;
     for (std::deque<int>::const_iterator facet = facets.begin(); facet != facets.end(); facet++) {
-      stl_out.facet_start[facet - facets.begin()] = stl_in.facet_start[*facet];
-      stl_facet_stats(&stl_out, stl_in.facet_start[*facet], first);
+      stl_out->facet_start[facet - facets.begin()] = stl_in->facet_start[*facet];
+      stl_facet_stats(stl_out, stl_in->facet_start[*facet], first);
       first = 0;
     }
     
-    std::stringstream ss;
-    ss << argv[1] << ".part" << namecounter << ".stl";
-    stl_write_binary(&stl_out, ss.str().c_str(), "stlsplit");
-    stl_exit_on_error(&stl_out);
-    stl_close(&stl_out);
+    parts.push_back(stl_out);
   }
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " file.stl" << std::endl;
+    return 1;
+  }
+  stl_file stl_in;
+  stl_open(&stl_in, argv[1]);
+  stl_exit_on_error(&stl_in);
+  
+  std::vector<stl_file*> parts = stl_split(&stl_in);
+  
+  unsigned int namecounter = 0;
+  
+  for (std::vector<stl_file*>::const_iterator part = parts.begin(); part != parts.end(); part++) {
+    std::stringstream ss;
+    ss << argv[1] << ".part" << ++namecounter << ".stl";
+    stl_write_binary(*part, ss.str().c_str(), "stlsplit");
+    stl_exit_on_error(*part);
+    stl_close(*part);
+    delete *part;
+  }
+  
+  stl_close(&stl_in);
 }
 
