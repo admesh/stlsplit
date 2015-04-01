e815ac043e36912d052575a855e9dfd9d682b3f0
Miro Hrončok <miro@hroncok.cz>
2015-04-01 22:57:56 +0200

    Use premake

---
+++ premake4.lua
@@ -0,0 +1,35 @@
+
+local action = _ACTION or ""
+
+solution "stlsplit"
+	location ( "." )
+	configurations { "Debug", "Release" }
+	platforms {"native", "x64", "x32"}
+  
+	configuration "Debug"
+		defines { "DEBUG" }
+		flags { "Symbols", "ExtraWarnings"}
+
+	configuration "Release"
+		defines { "NDEBUG" }
+		flags { "Optimize", "ExtraWarnings"}    
+
+
+	project "lib"
+		language "C++"
+		kind "SharedLib"
+		files { "stlsplit.cpp", "*.h" }
+		targetdir("build")
+		linkoptions { "-Wl,-soname,libstlsplit.so.1" }
+		targetname ("stlsplit")
+		targetextension (".so.1")
+
+	project "cli"
+		kind "ConsoleApp"
+		language "C++"
+		linkoptions { "-Lbuild" }
+		links { ":libstlsplit.so.1", "admesh" }
+		files { "cli.cpp" }
+		targetdir("build")
+		targetname ("stlsplit")
+
