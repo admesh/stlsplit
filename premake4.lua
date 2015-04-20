87e10bc39a0588b5e86b01513bbc8b562c7852d6
Miro Hrončok <miro@hroncok.cz>
2015-04-20 05:25:33 +0200

    Support Mac OS X

--- premake4.lua
+++ premake4.lua
@@ -2,9 +2,10 @@
 
 solution "stlsplit"
 	location ( "." )
+	targetdir("build")
 	configurations { "Debug", "Release" }
 	platforms {"native", "x64", "x32"}
-  
+
 	configuration "Debug"
 		defines { "DEBUG" }
 		flags { "Symbols", "ExtraWarnings"}
@@ -18,17 +19,22 @@ solution "stlsplit"
 		language "C++"
 		kind "SharedLib"
 		files { "stlsplit.cpp", "*.h" }
-		targetdir("build")
-		linkoptions { "-Wl,-soname,libstlsplit.so.1" }
 		targetname ("stlsplit")
-		targetextension (".so.1")
+		configuration { "linux" }
+			targetextension (".so.1")
+			linkoptions { "-Wl,-soname,libstlsplit.so.1" }
+			postbuildcommands { "ln -sf libstlsplit.so.1 build/libstlsplit.so" }
+			--cleancommands { "rm build/libstlsplit.so || :" }
+		configuration { "macosx" }
+			targetextension (".1.dylib")
+			postbuildcommands { "ln -sf libstlsplit.1.dylib build/libstlsplit.dylib" }
+			--cleancommands { "rm build/libstlsplit.dylib || :" }
+			links { "admesh" }
 
 	project "cli"
 		kind "ConsoleApp"
 		language "C++"
 		linkoptions { "-Lbuild" }
-		links { ":libstlsplit.so.1", "admesh" }
 		files { "cli.cpp" }
-		targetdir("build")
 		targetname ("stlsplit")
-
+		links { "stlsplit", "admesh" }
