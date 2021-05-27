bf37eff74d30ce47ab493fdf5442ec47a65e42eb
Udi Finkelstein <github@udifink.com>
2021-05-27 17:05:23 +0300

    merged the static lib into the exiksting LUA file as a separate project

--- premake4.lua
+++ premake4.lua
@@ -14,19 +14,26 @@ solution "stlsplit"
 		defines { "NDEBUG" }
 		flags { "Optimize", "ExtraWarnings"}    
 
+        project "static-lib"
+                language "C++"
+                files { "stlsplit.cpp", "*.h" }
+                targetname ("stlsplit")
+                links { "admesh" }
+                kind "StaticLib"
 
 	project "lib"
 		language "C++"
-		kind "SharedLib"
 		files { "stlsplit.cpp", "*.h" }
 		targetname ("stlsplit")
 		links { "admesh" }
 		configuration { "linux" }
+			kind "SharedLib"
 			targetextension (".so.1")
 			linkoptions { "-Wl,-soname,libstlsplit.so.1" }
 			postbuildcommands { "ln -sf libstlsplit.so.1 build/libstlsplit.so" }
 			--cleancommands { "rm build/libstlsplit.so || :" }
 		configuration { "macosx" }
+			kind "SharedLib"
 			targetextension (".1.dylib")
 			postbuildcommands { "ln -sf libstlsplit.1.dylib build/libstlsplit.dylib" }
 			--cleancommands { "rm build/libstlsplit.dylib || :" }
